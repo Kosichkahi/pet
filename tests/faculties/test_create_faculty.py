a7e9c6880b3042a129b93eaf005206af715de544
iuliia <marchenkoiuliia@gmail.com>
2022-10-09 13:33:01 +0300

    add tests sceleton

---
+++ tests/faculties/test_create_faculty.py
@@ -0,0 +1,14 @@
+class TestCreateFaculty:
+    """Проверка создания нового факультета"""
+
+    def test_create_faculty__success(self):
+        """Успешное создание нового факультета"""
+        pass
+
+    def test_create_faculty__unique_name(self):
+        """
+        Попытка создания факультета с именем, которое уже занято
+        Ожидается:
+            соответствующая ошибка
+        """
+        pass
